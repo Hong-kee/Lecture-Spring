c5a7ef49784d346590c97cd4d7c0e5d99401ec45
Hongki Kim <69206748+Hong-kee@users.noreply.github.com>
2022-02-07 09:48:43 +0900

    Update ForLoopMain.java

--- HomeWork/2022-02-03/ForLoopMain.java
+++ HomeWork/2022-02-03/ForLoopMain.java
@@ -6,39 +6,41 @@ public class ForLoop1 extends ForLoopData{
 
     public static void main(String[] args) throws IOException {
 
+        ForLoopData forLoopData = new ForLoopData();
+
         while(true) {
-            inputData();
-            if (playTheForLoop()) return;
+            forLoopData.inputData();
+            if (playTheForLoop(forLoopData)) return;
         }
+
     }
-  
-    private static boolean playTheForLoop() {
+    public static boolean playTheForLoop(ForLoopData forLoopData) {
         switch (getPlayNumber()) {
             case "0":
                 System.out.println("종료!");
                 return true;
-            case "1-1": playOne(); break;
-            case "1-2": playTwo(); break;
-            case "1-3": playThree(); break;
-            case "1-4": playFour(); break;
-            case "1-5": playFive(); break;
-            case "1-6": playSix(); break;
-            case "1-7": playSeven(); break;
+            case "1-1": forLoopData.playOne(); break;
+            case "1-2": forLoopData.playTwo(); break;
+            case "1-3": forLoopData.playThree(); break;
+            case "1-4": forLoopData.playFour(); break;
+            case "1-5": forLoopData.playFive(); break;
+            case "1-6": forLoopData.playSix(); break;
+            case "1-7": forLoopData.playSeven(); break;
 
-            case "2-1": playEight(); break;
-            case "2-2": playNine(); break;
-            case "2-3": playTen(); break;
-            case "2-4": playEleven(); break;
-            case "2-5": playTwelve(); break;
-            case "2-6": playThirteen(); break;
-            case "2-7": playFourteen(); break;
-            case "2-8": playFifteen(); break;
-            case "2-9": playSixteen(); break;
-            case "2-10": playSeventeen(); break;
+            case "2-1": forLoopData.playEight(); break;
+            case "2-2": forLoopData.playNine(); break;
+            case "2-3": forLoopData.playTen(); break;
+            case "2-4": forLoopData.playEleven(); break;
+            case "2-5": forLoopData.playTwelve(); break;
+            case "2-6": forLoopData.playThirteen(); break;
+            case "2-7": forLoopData.playFourteen(); break;
+            case "2-8": forLoopData.playFifteen(); break;
+            case "2-9": forLoopData.playSixteen(); break;
+            case "2-10": forLoopData.playSeventeen(); break;
 
-            case "1-8": playEighteen(); break;
-            case "1-9": playNineteen(); break;
-            case "1-10": playTwenty(); break;
+            case "1-8": forLoopData.playEighteen(); break;
+            case "1-9": forLoopData.playNineteen(); break;
+            case "1-10": forLoopData.playTwenty(); break;
 
             default: System.out.println("그런 번호는 없습니다.");
         }
