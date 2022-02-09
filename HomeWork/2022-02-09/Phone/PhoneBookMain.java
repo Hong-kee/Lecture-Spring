275c64c9c98344d458669e988bf01c96d011c3f8
Hongki Kim <69206748+Hong-kee@users.noreply.github.com>
2022-02-09 17:12:59 +0900

    PhoneBookMain
    PhoneBookMain

---
+++ HomeWork/2022-02-09/Phone/PhoneBookMain.java
@@ -0,0 +1,27 @@
+package kosta.oop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PhoneBookMain {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final PhoneBookManager phoneBookManager = new PhoneBookManager();
+
+    public static void main(String[] args) throws IOException {
+
+        //number == 4이면 프로그램 종료
+        while (true) {
+            System.out.println("1 : 추가 / 2 : 전체 출력 / 3 : 검색 / 4 : 종료");
+            int number = Integer.parseInt(br.readLine());
+
+            switch (number) {
+                case 1 -> phoneBookManager.addInformation();
+                case 2 -> phoneBookManager.printAll();
+                case 3 -> phoneBookManager.searchPerson();
+                case 4 -> System.out.print("종료!");
+            }
+        }
+    }
+}
